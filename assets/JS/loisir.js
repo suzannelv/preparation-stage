778e23c04d7ab9108afc89250c6e5b9e8b5614af
suzannelv <lvmeijuan9011@gmail.com>
2022-12-06 16:43:42 +0100

    mis à jour

--- assets/JS/loisir.js
+++ assets/JS/loisir.js
@@ -1,38 +1,37 @@
-// class Tricot {
-//       constructor( title, image, makeDuration )
+class Tricot {
+      constructor( title, image, makeDuration )
   
-//       {this.title = title;
-//       this.image = image;
-//       this.makeDuration = makeDuration; 
-//       }
-//     }
+      {this.title = title;
+      this.image = image;
+      this.makeDuration = makeDuration; 
+      }
+    }
 
-//     const tricots=[
-//       new Tricot ("Chien Loulou", "./images/tricot/chien.jpg", "7h"),
-//       new Tricot ("Petite couple", "./images/tricot/couple.jpg", "6h"),
-//       new Tricot ("Tony-Tony Chopper", "./images/tricot/Chopper.jpg", "10h"), 
-//       new Tricot ("Moutons", "./images/tricot/mouton.jpg", "3h"),
-//       new Tricot ("Porcinet", "./images/tricot/porcinet.jpg", "4h"),
-//       new Tricot ("Winnie", "./images/tricot/winnie.jpg","4h")
-//   ]
+    const tricots=[
+      new Tricot ("Chien Loulou", "chien.jpg", "7h"),
+      new Tricot ("Petite couple", "couple.jpg", "6h"),
+      new Tricot ("Tony-Tony Chopper", "Chopper.jpg", "10h"), 
+      new Tricot ("Moutons", "mouton.jpg", "3h"),
+      new Tricot ("Porcinet", "porcinet.jpg", "4h"),
+      new Tricot ("Winnie", "winnie.jpg","4h")
+  ]
 
 
-// let tricotAdd = "";
+let tricotAdd = "";
 
-// for (let knitting of tricots ) {
-// tricotAdd += `
-// <div class="col mt-5">
-// <div class="card shadow-sm">
-// <img src="${image}" alt="" class="img-fluid rounded mx-auto d-block">
-// <div class="card-body">
-//   <button class="card-text border-0 rounded-pill text-light fw-semibold"> Loulou</button>
-//     <p class="text-center mt-4">Temps de fabrication: 7h</p>
-//   </div>
-// </div>
-// </div>
+for (let knitting of tricots ) {
+tricotAdd += `
+<div class="col mt-5">
+<div class="card shadow-sm">
+<img src="./images/tricot/${knitting.image}" alt="image${knitting.image}" class="img-fluid rounded mx-auto d-block photo-tricot">
+<div class="card-body">
+  <button class="card-text border-0 rounded-pill text-light fw-semibold"> Loulou</button>
+    <p class="text-center mt-4">Temps de fabrication: 7h</p>
+  </div>
+</div>
+</div>
+ `
 
-// // `
-
-// }
-// document.getElementById("knitting").innerHTML=tricotAdd 
+}
+document.getElementById("knitting").innerHTML=tricotAdd 
 
