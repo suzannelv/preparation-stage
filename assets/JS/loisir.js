11bf02884c83545d21a23419f02a935cf34baf8c
suzannelv <lvmeijuan9011@gmail.com>
2022-12-06 20:35:36 +0100

    bg color

--- assets/JS/loisir.js
+++ assets/JS/loisir.js
@@ -11,8 +11,8 @@ class Tricot {
       new Tricot ("Chien Loulou", "chien.jpg", "7h"),
       new Tricot ("Petite couple", "couple.jpg", "6h"),
       new Tricot ("Tony-Tony Chopper", "Chopper.jpg", "10h"), 
-      new Tricot ("Moutons", "mouton.jpg", "3h"),
       new Tricot ("Porcinet", "porcinet.jpg", "4h"),
+      new Tricot ("Moutons", "mouton.jpg", "3h"),
       new Tricot ("Winnie", "winnie.jpg","4h")
   ]
 
@@ -25,7 +25,7 @@ tricotAdd += `
 <div class="card shadow-sm">
 <img src="./images/tricot/${knitting.image}" alt="image${knitting.image}" class="img-fluid rounded mx-auto d-block photo-tricot">
 <div class="card-body">
-  <button class="card-text border-0 rounded-pill text-light fw-semibold"> Loulou</button>
+  <button class="card-text border-0 rounded-pill text-light btn-knitting fw-semibold">${knitting.title}</button>
     <p class="text-center mt-4">Temps de fabrication: 7h</p>
   </div>
 </div>
